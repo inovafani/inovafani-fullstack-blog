2c94d256560edadcf768666e7a007d70e5186c26
inovafani <inoveka@gmail.com>
2025-08-09 23:33:20 +0700

    Finishing the WYSIWYG

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -8,17 +8,28 @@
 
     <div>
         <label>Judul</label>
-        <input type="text" name="judul" value="{{old('judul')}}">
+        <input type="text" name="title" value="{{old('title')}}">
     </div>
-</form>
 
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-<script>
-    tinymce.init({
-    selector: 'textarea[name="content"]',
-    height: 400,
-    menubar: false,
-    plugins: 'link image code lists',
-    toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist | link image | code'
-    })
-</script>
+    <div>
+        <label>Konten</label>
+        <textarea name="content">{{old('content')}}</textarea>
+    </div>
+
+    <button type="submit">Simpan</button>
+</form>
+@endsection
+
+@push('scripts')
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+        selector: 'textarea[name="content"]',
+        height: 400,
+        menubar: false,
+        plugins: 'link image code lists',
+        toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist | link image | code'
+        })
+    </script>
+@endpush
+
