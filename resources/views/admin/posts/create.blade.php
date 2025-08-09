73a82ca21541d1d4e15d330832a9dfacada00128
inovafani <inoveka@gmail.com>
2025-08-09 23:30:28 +0700

    Add the WYSIWYG

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Buat Post Baru</h1>
+
+<form method="POST" action="{{route('posts.store')}}">
+    @csrf
+
+    <div>
+        <label>Judul</label>
+        <input type="text" name="judul" value="{{old('judul')}}">
+    </div>
+</form>
+
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+    selector: 'textarea[name="content"]',
+    height: 400,
+    menubar: false,
+    plugins: 'link image code lists',
+    toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist | link image | code'
+    })
+</script>
